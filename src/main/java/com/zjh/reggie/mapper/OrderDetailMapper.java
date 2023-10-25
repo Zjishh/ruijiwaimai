228d395d3f2b8436e1ff8d85079633893368fa88
38580 <385802188@qq.com>
2023-10-25 21:42:10 +0800

    10.25 晚上 课程完结 功能待开发

---
+++ src/main/java/com/zjh/reggie/mapper/OrderDetailMapper.java
@@ -0,0 +1,12 @@
+package com.zjh.reggie.mapper;
+
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjh.reggie.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+
+}
---
+++ src/main/java/com/zjh/reggie/service/OrderDetailService.java
@@ -0,0 +1,9 @@
+package com.zjh.reggie.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zjh.reggie.entity.OrderDetail;
+
+public interface OrderDetailService extends IService<OrderDetail> {
+
+}
